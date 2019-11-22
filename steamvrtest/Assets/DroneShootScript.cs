1e8489323c0101447f40909a4482b2ecaea65196
Brandon Carlisle <brandontcarlisle@gmail.com>
2019-11-21 19:57:49 -0800

    Levels

---
+++ steamvrtest/Assets/DroneShootScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneShootScript : MonoBehaviour
+{
+    public GameObject shootFrom;
+    public GameObject projectileType;
+    private float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 2f)
+        {
+            Shoot();
+            timer = 0f;
+        }
+    }
+
+    void Shoot()
+    {
+        //var proj = Instantiate(projectileType, shootFrom.transform.position, Quaternion.identity);
+     
+    }
+}
---
+++ steamvrtest/Assets/droneLaserScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class droneLaserScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 200.0f;
+    private float timer = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 5f)
+            Destroy(gameObject);
+
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
